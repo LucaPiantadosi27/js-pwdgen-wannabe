77b230fd3d0b51a975477f8a1e8c9c20671bd137
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-01-28 19:15:58 +0100

    js

---
+++ js/master.js
@@ -0,0 +1,5 @@
+const firstName = prompt("Inserisci il tuo nome!");
+const lastName = prompt("Ora inserisci il tuo cognome!");
+const favouriteColour = prompt("Ora il tuo colore preferito!");
+
+document.querySelector("h2").innerHTML = `${firstName}${lastName}${favouriteColour}76`;
